083b7372a1bda31c1b98787a4f0a20fe389438d8
Jimmy Lee <jimlee604@gmail.com>
2025-07-01 01:54:11 -0700

    navigation works

--- play_fab/my-app/src/App.js
+++ play_fab/my-app/src/App.js
@@ -1,18 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import GameScreenComponent from './GameScreenComponent.js'
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,  
+  Link,
+  Navigate,
+  useNavigate
+} from "react-router-dom";
+import React, { useEffect, useState} from 'react';
 
-function App() {
+
+<Route exact path='.' component={GameScreenComponent} />
+
+const HomeComponent = () => {
+  const navigate = useNavigate();
   return (
-    <body style={{backgroundColor:'#e38578'}}>
-    <div className='center'>
-      <div>
-      <h1>Title</h1>
-      <div className='height30vh'></div>
-      <button>Get Started</button>
+    <body style={{ backgroundColor: '#e38578' }}>
+      <div className='height_10vh'></div>
+      <div className='center'>
+        <div>
+          <h1>Welcome to <b>Flesh and Blood Lite</b></h1>
+          <div className='height_30vh'></div>
+          <div className='center'>
+          <button onClick={() => navigate('/game')} class='start_button'>Play Game</button>
+          </div>
+        </div>
       </div>
-    </div>
     </body>
   );
+}
+
+// Home Page
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+              <Route exact path='/' element={<HomeComponent />}></Route>
+              <Route exact path='/game' element={<GameScreenComponent />}></Route>
+            </Routes>
+            </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ play_fab/my-app/src/GameScreenComponent.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+// Game Page
+const GameScreenComponent = () => {
+  return (
+    <body style={{ backgroundColor: 'green' }}>
+      <div className='height_10vh'></div>
+      <div className='center'>
+        <div>
+          <h1>Welcome to the Game Screen</h1>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default GameScreenComponent;
