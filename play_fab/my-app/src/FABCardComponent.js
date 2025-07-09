f12e331af87e68aa6610b21100c40f93abe90869
Jimmy Lee <jimlee604@gmail.com>
2025-07-09 15:07:08 -0700

    more switches

--- play_fab/my-app/src/FABCardComponent.js
+++ play_fab/my-app/src/FABCardComponent.js
@@ -10,8 +10,8 @@ const FABCardComponent = (props) => {
         <div>
             <p>
                 <b>Cost:</b>{card.cost}
-                 {/* || <b>id:</b>{card.id} */}
-                 </p>
+                {/* || <b>id:</b>{card.id} */}
+            </p>
             <p><b>Pitch:</b>{card.pitch}</p>
             <p><b>Attack:</b>{card.attack}</p>
             <p><b>Block:</b>{card.block}</p>
@@ -25,38 +25,47 @@ const FABCardComponent = (props) => {
 
     const { selectedCardValue, setSelectedCardValue } = useSelectedCard()
     const handleClick = () => {
-        if (turnStepValue === "Select Attack" || turnStepValue === "Select Attack 2") {
-            setSelectedCardValue(card)
-            console.log(card)
-        } else {
-            let nextPitchValue = new Set(pitchCardsSelectedValue)
-            console.log(attackingCardValue)
-            console.log(card)
-            if (attackingCardValue && attackingCardValue !== card) {
-                if (pitchCardsSelectedValue.has(card)) {
-                    nextPitchValue.delete(card)
-                    setPitchAmountValue(pitchAmountValue - card.pitch)
-                } else {
-                    nextPitchValue.add(card)
-                    setPitchAmountValue(pitchAmountValue + card.pitch)
+        switch (turnStepValue) {
+            case "Select Attack":
+            case "Select Attack 2":
+                setSelectedCardValue(card)
+                console.log(card)
+                break;
+            case "Pitch":
+            case "Pitch 2":
+                let nextPitchValue = new Set(pitchCardsSelectedValue)
+                console.log(attackingCardValue)
+                console.log(card)
+                if (attackingCardValue && attackingCardValue !== card) {
+                    if (pitchCardsSelectedValue.has(card)) {
+                        nextPitchValue.delete(card)
+                        setPitchAmountValue(pitchAmountValue - card.pitch)
+                    } else {
+                        nextPitchValue.add(card)
+                        setPitchAmountValue(pitchAmountValue + card.pitch)
+                    }
+                    setPitchCardsSelectedValue(nextPitchValue)
                 }
-                setPitchCardsSelectedValue(nextPitchValue)
-            }
+                break;
+            case "PlayerAttack":
+                break;
+            default:
+                return;
         }
-    };
-    const highlightRed = (attackingCardValue && attackingCardValue === card)
-    const highlightBlue = (turnStepValue === "Pitch" || turnStepValue === "Pitch 2" || turnStepValue === "Player Attack") && pitchCardsSelectedValue.has(card)
-    const highlightYellow = (turnStepValue === "Select Attack" || turnStepValue === "Select Attack 2") && (selectedCardValue && selectedCardValue.id == card.id)
-    return (
-        card.playerOwned ?
-            (<button className={("FABCard center" + (highlightRed ? " highlight_red" : (highlightBlue ? " highlight_blue" : (highlightYellow ? " highlight_yellow" : ""))))} onClick={handleClick}>
-                {innerComponent}
-            </button>)
-            :
-            (<div className="FABCard center">
-                {innerComponent}
-            </div>)
-    );
+};
+const highlightRed = (attackingCardValue && attackingCardValue === card)
+const highlightBlue = (turnStepValue === "Pitch" || turnStepValue === "Pitch 2" || turnStepValue === "Player Attack") && pitchCardsSelectedValue.has(card)
+const highlightYellow = (turnStepValue === "Select Attack" || turnStepValue === "Select Attack 2") && (selectedCardValue && selectedCardValue.id == card.id)
+return (
+    card.playerOwned ?
+        (<button className={("FABCard center" + (highlightRed ? " highlight_red" : (highlightBlue ? " highlight_blue" : (highlightYellow ? " highlight_yellow" : ""))))} onClick={handleClick}>
+            {innerComponent}
+        </button>)
+        :
+        (<div className="FABCard center">
+            {innerComponent}
+        </div>)
+);
 }
 
 export default FABCardComponent;
